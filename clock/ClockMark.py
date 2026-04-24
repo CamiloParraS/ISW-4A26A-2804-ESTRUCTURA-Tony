63c26992afd0e6d388e63b0513da807490c759c7
CamiloParraS <juanparracsfn@gmail.com>
2026-04-23 21:35:35 -0500

    I forgor

--- clock/ClockMark.py
+++ clock/ClockMark.py
@@ -8,32 +8,8 @@ MarkType = TypeVar("MarkType")
 
 @dataclass(slots=True)
 class ClockMark(Generic[MarkType]):
-    """A single node in the circular doubly linked list."""
+    """representa una marca del reloj (hora, minuto o segundo)"""
 
-    data: MarkType
-    prev: ClockMark[MarkType] | None = None
-    next: ClockMark[MarkType] | None = None
-
-    @property
-    def label(self) -> MarkType:
-        return self.data
-
-    @label.setter
-    def label(self, value: MarkType) -> None:
-        self.data = value
-
-    @property
-    def next_mark(self) -> ClockMark[MarkType] | None:
-        return self.next
-
-    @next_mark.setter
-    def next_mark(self, value: ClockMark[MarkType] | None) -> None:
-        self.next = value
-
-    @property
-    def previous_mark(self) -> ClockMark[MarkType] | None:
-        return self.prev
-
-    @previous_mark.setter
-    def previous_mark(self, value: ClockMark[MarkType] | None) -> None:
-        self.prev = value
+    label: MarkType
+    next_mark: ClockMark[MarkType] | None = None
+    previous_mark: ClockMark[MarkType] | None = None
